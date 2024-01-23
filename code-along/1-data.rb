ac3be274f784e52af95e0ec88321207c53febf47
Rob Vogel <rob.vogel@kellogg.northwestern.edu>
2024-01-23 15:02:07 +0000

    First couple class exercises

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -17,3 +17,39 @@
 # Combine strings and variables
 
 # String manipulation
+
+# #put the number 2on the screen
+# puts 2
+
+# #put #5 on screen
+# puts 5
+
+# #add 5 and 2 
+# puts 5+2
+
+# # put tacos on screen
+# puts "tacos"
+
+# #store 2 and 5 into x and y in memory
+# x = 2
+# y = 5
+
+# #put the value of x on screen
+# puts x
+
+# #put the value of y on screen
+# puts y
+
+# # put the product of x and y on screen
+# puts x * y 
+
+#store my favorite thing in memory
+favorite_thing = "tacos"
+
+#write favorite thing to screen
+puts favorite_thing
+
+#write my favorite thing backwards
+puts favorite_thing.reverse
+#called a method, when it calls upon the variable favorite_thing
+
--- labs/1-data.rb
+++ labs/1-data.rb
@@ -15,3 +15,25 @@
 # rand(1..6)
 # Log output to the screen with puts:
 # puts "Hello, world"
+
+#Rob's efforts
+
+#store a random # in memory for first die
+D1 = rand(1..6)
+#store a random # in memory for 2nd die
+D2 = rand(1..6)
+#store the total in memory
+Total = D1 + D2
+
+#write the first die to screen in user friendly way
+puts D1
+
+#write 2nd die to screen in user friendly way
+puts D2
+
+#write total to screen in user friendly way
+puts Total
+
+puts "D1 =  #{D1}"
+puts "D2 =  #{D2}"
+puts "Total =  #{Total}"
